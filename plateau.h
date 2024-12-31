abc150d082c19a5b56dead20f4df340cfd72e1ba
PLFR23456 <140762910+PLFR23456@users.noreply.github.com>
2024-12-31 15:02:31 +0100

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -5,12 +5,12 @@
 typedef struct deplacement {  // Ajoutez le mot-clé 'struct'
     int x;
     int y;
-    int info;
+    int info; //-2 = propre pion (non attaquable) -1 = vide, 0 = coup normal, 10 à 25+ -> pion mangé 1X = pion de couleur 0 , 2X pion de couleur 1
     struct deplacement* next;  // Utilisez 'struct deplacement*' au lieu de 'deplacement*'
 } deplacement;  // Ajoutez un typedef
 
 void affiche(piece plateau[TAILLE][TAILLE], deplacement* liste);
 void initialise_plateau(piece plateau[TAILLE][TAILLE]);
-void addtolist(int x, int y, int info,deplacement* head, piece plateau[TAILLE][TAILLE]);
+void creer_partie_personnalisee(piece tableau[TAILLE][TAILLE]);
 void promotion(int x, int y, piece plateau[TAILLE][TAILLE]);
 #endif
